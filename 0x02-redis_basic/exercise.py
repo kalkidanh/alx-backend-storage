07a440fdb20c6e903254360d44e5cdbbfa7f9310
kalkidanh <kalkidanh89@gmail.com>
2023-08-16 17:03:47 +0000

    original type recovered

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ Class that stores a Redis client as a private variable"""
 
 from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
 
@@ -24,3 +25,20 @@ class Cache():
         generate_random_key = str(uuid4())
         self._redis.set(generate_random_key, data)
         return generate_random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """ Method that takes a key string argument and
+        an optional Callable argument"""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key):
+        """ Method that automatically parametrizes Cache.get"""
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key):
+        """ Method that automatically parametrizes Cache.get"""
+        return self._redis.get(key, int)
